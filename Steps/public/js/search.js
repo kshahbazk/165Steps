12643507c8fdb307cc04e81ff612cbce4d186415
shahbazkhan <kshahbazk@yahoo.com>
2015-11-23 23:48:57 -0800

    Created js class

---
+++ Steps/public/js/search.js
@@ -0,0 +1,20 @@
+/**
+ * Created by shahbazkhan on 11/23/15.
+ */
+
+function search() {
+
+    var searchedWord = $("#searchTerm").val();
+
+    var ReturnedResults = Parse.Object.extend("service");
+
+    var query = new Parse.Query(ReturnedResults);
+
+    query.whereMatches("name", "(" + searchedWord + ")", "i").find({
+        success: function(comments) {
+            // comments now contains the comments for myPost
+        }
+    });
+
+
+}
