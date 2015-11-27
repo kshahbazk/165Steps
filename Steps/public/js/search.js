04d9fa3ae564f20076ebb4b51b713405773cea9a
shahbazkhan <kshahbazk@yahoo.com>
2015-11-26 21:40:06 -0800

    Added cloud coude search

--- Steps/public/js/search.js
+++ Steps/public/js/search.js
@@ -7,10 +7,17 @@
 //    alert('ok');
 //});​
 
+
 var searchedWord = $("#searchTerm").val();
 
 Parse.Cloud.run('queryServices', { serviceQuery: searchedWord }, {
     success: function(results) {
+        alert("Successfully retrieved " + results.length + " results.");
+        // Do something with the returned Parse.Object values
+        for (var i = 0; i < results.length; i++) {
+            var object = results[i];
+            alert(object.id + ' - ' + object.get('name'));
+        }
 
     },
     error: function(error) {
