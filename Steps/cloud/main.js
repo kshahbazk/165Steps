e567b89ae1cb88ebca9496ba56746bd873769bcf
fernandoabolafio <fernandohsabolafio@gmail.com>
2015-12-01 16:31:35 -0800

    backend stuff

--- Steps/cloud/main.js
+++ Steps/cloud/main.js
@@ -22,19 +22,72 @@ Parse.Cloud.define("queryServices", function(request, response) {
   });
 });
 
-/*Parse.Cloud.beforeSave("service", function(request, response) {
+Parse.Cloud.define("getUserId", function(request, response)
+{
+    //Example where an objectId is passed to a cloud function.
+    var id = request.params.objectId;
+
+    //When getUser(id) is called a promise is returned. Notice the .then this means that once the promise is fulfilled it will continue. See getUser() function below.
+    getUser(id).then
+    (
+        //When the promise is fulfilled function(user) fires, and now we have our USER!
+        function(user)
+        {
+            response.success(user);
+        }
+        ,
+        function(error)
+        {
+            response.error(error);
+        }
+    );
+
+});
+
+function getUser(userId)
+{
+    Parse.Cloud.useMasterKey();
+    var userQuery = new Parse.Query(Parse.User);
+    userQuery.equalTo("objectId", userId);
+
+    //Here you aren't directly returning a user, but you are returning a function that will sometime in the future return a user. This is considered a promise.
+    return userQuery.first
+    ({
+        success: function(userRetrieved)
+        {
+            //When the success method fires and you return userRetrieved you fulfill the above promise, and the userRetrieved continues up the chain.
+            return userRetrieved;
+        },
+        error: function(error)
+        {
+            return error;
+        }
+    });
+}
+
+Parse.Cloud.beforeSave("service", function(request, response) {
 
   var _apiKey = "key-f630b4300fcb468b8791446715741d07";
   var _domainName = "sandbox849033cc1d2d4cac8be3884abbb52a4c.mailgun.org";
+
+  var email;
+
+  if(request.object.get("buying")){
+    email = request.object.get("serviceBuyer").get("email");
+  }else{
+    email = request.object.get("serviceSeller").get("email");
+  }
+
+
 
   Parse.Cloud.httpRequest({
       method: "POST",
       url: "https://api:" + _apiKey + "@api.mailgun.net/v2/" + _domainName + "/messages",
       body: {
-          to: "fernandohsabolafio@gmail.com",
+          to: email,
           from: "postmaster@sandbox849033cc1d2d4cac8be3884abbb52a4c.mailgun.org",
           subject: "Hello from Cloud Code!",
-          text: "Using Parse and Mailgun is great!"
+          text: "A user just applied for one of your jobs"
       },
       success: function(httpResponse) {
         console.log(httpResponse);
@@ -44,10 +97,13 @@ Parse.Cloud.define("queryServices", function(request, response) {
         console.error(httpResponse);
         response.error("Uh oh, something went wrong");
       }
+
     });
+    response.success();
+});
 
 
-/*  var Mailgun = require('mailgun');
+ /*var Mailgun = require('mailgun');
   Mailgun.initialize('https://api.mailgun.net/v3/steps165.parseapp.com', 'key-f630b4300fcb468b8791446715741d07');
 
   Mailgun.sendEmail({
@@ -64,6 +120,4 @@ Parse.Cloud.define("queryServices", function(request, response) {
       console.error(httpResponse);
       response.error("Uh oh, something went wrong");
     }
-  });
-
-});*/
+  });*/
---
+++ Steps/public/js/searchTest.js
@@ -0,0 +1,39 @@
+
+function getServices () {
+  var services = Parse.Object.extend("service");
+  var mainQuery = new Parse.Query(services);
+
+
+  mainQuery.equalTo("serviceState", "open");
+
+
+
+
+  mainQuery.find({
+    success: function (results){
+      $("#services-result-list").html("");
+      var template = Handlebars.compile($("#services-result-template").html());
+      $(results).each(function (i,e){
+        var q = e.toJSON();
+
+
+        $("#services-result-list").append(template(q));
+      })
+
+    },
+    error: function(error){
+      console.log(error.message);
+    }
+  })
+
+}
+
+function linkToServicePage(serviceId){
+ console.log(serviceId);
+ sessionStorage.setItem("serviceId", serviceId);
+ document.location.href = "servicePage.html";
+}
+
+$(document).ready(function () {
+  getServices();
+})
